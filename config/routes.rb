37b740a387e055ece1eb09873ccf4c68f745e281
Mariana Simon <simon.mari1@gmail.com>
2022-03-11 17:12:01 -0500

    Add listing and welcome routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  get 'listings/new' => 'listings#new'
+  get 'listings/:id' => 'listings#show', as: :listing
+  get '/listings' => 'listings#index'
+  post '/listings' => 'listings#create'
+ 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
